1642cea03b4df85ea72b98ec724d384e5aad375d
MBrian Website <muhammad.brian.27@gmail.com>
2022-12-11 17:02:35 +0700

    SPLIT NAVBAR COMPONENT AND HOME VIEW

---
+++ src/components/home/Categories.vue
@@ -0,0 +1,85 @@
+<script setup>
+import Card from "./../../components/home/categories/Card.vue";
+
+import { ref } from 'vue';
+
+const cards = ref([
+    { title: 'Mobile UI Kit', image: 'categories-1.jpg', items: 731 },
+    { title: 'Fonts', image: 'categories-2.jpg', items: 657 },
+    { title: 'Icon Set', image: 'categories-3.jpg', items: 83559 },
+    { title: 'Web UI Kit', image: 'categories-4.jpg', items: 4500 }
+])
+
+</script>
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12">
+        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">
+            Top Categories
+        </h2>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <template v-for="card in cards">
+                <Card :title="card.title" :items="card.items" :image="card.image" />
+            </template>
+            <!-- <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
+                <div class="overflow-hidden border border-gray-200 rounded-xl">
+                    <a href="details.html">
+                        <div class="m-4 overflow-hidden rounded-xl">
+                            <img alt="Placeholder" class="block w-full h-auto" src="@/assets/img/categories-2.jpg" />
+                        </div>
+                    </a>
+
+                    <header class="px-4 mb-4 leading-tight">
+                        <h1 class="text-lg">
+                            <a class="font-semibold text-black no-underline hover:underline" href="#">
+                                Fonts
+                            </a>
+                        </h1>
+                        <span class="block text-sm font-light text-gray-500 no-underline">
+                            657 items
+                        </span>
+                    </header>
+                </div>
+            </div>
+            <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
+                <div class="overflow-hidden border border-gray-200 rounded-xl">
+                    <a href="details.html">
+                        <div class="m-4 overflow-hidden rounded-xl">
+                            <img alt="Placeholder" class="block w-full h-auto" src="@/assets/img/categories-3.jpg" />
+                        </div>
+                    </a>
+
+                    <header class="px-4 mb-4 leading-tight">
+                        <h1 class="text-lg">
+                            <a class="font-semibold text-black no-underline hover:underline" href="#">
+                                Icon Set
+                            </a>
+                        </h1>
+                        <span class="block text-sm font-light text-gray-500 no-underline">
+                            83,559 items
+                        </span>
+                    </header>
+                </div>
+            </div>
+            <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
+                <div class="overflow-hidden border border-gray-200 rounded-xl">
+                    <a href="details.html">
+                        <div class="m-4 overflow-hidden rounded-xl">
+                            <img alt="Placeholder" class="block w-full h-auto" src="@/assets/img/categories-4.jpg" />
+                        </div>
+                    </a>
+
+                    <header class="px-4 mb-4 leading-tight">
+                        <h1 class="text-lg">
+                            <a class="font-semibold text-black no-underline hover:underline" href="#">
+                                Website UI Kit
+                            </a>
+                        </h1>
+                        <span class="block text-sm font-light text-gray-500 no-underline">
+                            4,500 items
+                        </span>
+                    </header>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</template>
---
+++ src/components/layouts/navbar/Logo.vue
@@ -0,0 +1,6 @@
+
+<template>
+    <a href="index.html" class="flex items-center">
+        <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Zullkit Logo" />
+    </a>
+</template>
