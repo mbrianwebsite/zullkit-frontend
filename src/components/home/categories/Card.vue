d8e6144697a3b23e192fd2c4ff35f9684d43ddf7
MBrian Website <muhammad.brian.27@gmail.com>
2022-12-15 04:44:32 +0700

    FIX IMAGE FOLDER

--- src/components/home/categories/Card.vue
+++ src/components/home/categories/Card.vue
@@ -7,7 +7,7 @@ defineProps({ title: String, image: String, items: Number });
         <div class="overflow-hidden border border-gray-200 rounded-xl">
             <RouterLink to="details.html">
                 <div class="m-4 overflow-hidden rounded-xl">
-                    <img alt="Placeholder" class="block w-full h-auto" :src="'./src/assets/img/' + image" />
+                    <img alt="Placeholder" class="block w-full h-auto" :src="'/img/' + image" />
                 </div>
             </RouterLink>
 
