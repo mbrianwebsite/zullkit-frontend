48b81ad2bc711cc3874d04d177188c963b07def4
MBrian Website <muhammad.brian.27@gmail.com>
2022-12-15 09:20:51 +0700

    SPLIT GALLERY COMPONENT

---
+++ src/components/detail/Gallery.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+const defaultImage = ref('gallery-1.png');
+const galleries = ref([
+    { id: 1, image: 'gallery-2.png' },
+    { id: 2, image: 'gallery-3.png' },
+    { id: 3, image: 'gallery-4.png' },
+    { id: 4, image: 'gallery-5.png' }
+]);
+
+</script>
+<template>
+    <section id="gallery">
+        <img :src="'/img/' + defaultImage" alt="" class="w-full mt-6 rounded-2xl">
+        <div class="grid grid-cols-4 gap-4 mt-4">
+            <template v-for="picture in galleries">
+                <div class="overflow-hidden cursor-pointer rounded-2xl">
+                    <img :src="'/img/' + picture.image" class="w-full" alt="">
+                </div>
+            </template>
+            <!-- <div class="overflow-hidden cursor-pointer rounded-2xl ring-2 ring-indigo-500">
+                <img src="/img/gallery-2.png" class="w-full" alt="">
+            </div>
+            <div class="overflow-hidden cursor-pointer rounded-2xl">
+                <img src="/img/gallery-3.png" class="w-full" alt="">
+            </div>
+            <div class="overflow-hidden cursor-pointer rounded-2xl">
+                <img src="/img/gallery-4.png" class="w-full" alt="">
+            </div>
+            <div class="overflow-hidden cursor-pointer rounded-2xl">
+                <img src="/img/gallery-5.png" class="w-full" alt="">
+            </div> -->
+        </div>
+    </section>
+</template>
--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,3 +1,6 @@
+<script setup>
+import Gallery from "@/components/detail/Gallery.vue"
+</script>
 <template>
     <div class="container p-2 mx-auto my-10 max-w-7xl">
         <div class="flex flex-row flex-wrap py-4">
@@ -6,23 +9,7 @@
                     RoboCrypto UI Kit
                 </h1>
                 <p class="text-gray-500">Build your next coin startup</p>
-                <section id="gallery">
-                    <img src="/img/gallery-1.png" alt="" class="w-full mt-6 rounded-2xl">
-                    <div class="grid grid-cols-4 gap-4 mt-4">
-                        <div class="overflow-hidden cursor-pointer rounded-2xl">
-                            <img src="/img/gallery-2.png" class="w-full" alt="">
-                        </div>
-                        <div class="overflow-hidden cursor-pointer ring-2 ring-indigo-500 rounded-2xl">
-                            <img src="/img/gallery-3.png" class="w-full" alt="">
-                        </div>
-                        <div class="overflow-hidden cursor-pointer rounded-2xl">
-                            <img src="/img/gallery-4.png" class="w-full" alt="">
-                        </div>
-                        <div class="overflow-hidden cursor-pointer rounded-2xl">
-                            <img src="/img/gallery-5.png" class="w-full" alt="">
-                        </div>
-                    </div>
-                </section>
+                <Gallery />
                 <section class="" id="orders">
                     <h1 class="mt-8 mb-3 text-lg font-semibold">About</h1>
                     <div class="text-gray-500">
