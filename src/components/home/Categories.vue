682602ce13fb9d27cb721e63701f46a0ea627d29
MBrian Website <muhammad.brian.27@gmail.com>
2022-12-15 22:04:38 +0700

    SET CATEGORIES API

--- src/components/home/Categories.vue
+++ src/components/home/Categories.vue
@@ -1,14 +1,30 @@
 <script setup>
 import Card from "@/components/home/categories/Card.vue";
+import { ref, onMounted } from 'vue';
+import axios from 'axios'
 
-import { ref } from 'vue';
+const categories = ref([])
 
-const cards = ref([
-    { title: 'Mobile UI Kit', image: 'categories-1.jpg', items: 731 },
-    { title: 'Fonts', image: 'categories-2.jpg', items: 657 },
-    { title: 'Icon Set', image: 'categories-3.jpg', items: 83559 },
-    { title: 'Web UI Kit', image: 'categories-4.jpg', items: 4500 }
-])
+async function getCategories() {
+    try {
+        const response = await axios.get('https://zullkit-backend.buildwithangga.id/api/categories')
+        // console.log(response.data)
+        categories.value = response.data.data.data
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+onMounted(() => {
+    getCategories();
+})
+
+// const cards = ref([
+//     { title: 'Mobile UI Kit', image: 'categories-1.jpg', items: 731 },
+//     { title: 'Fonts', image: 'categories-2.jpg', items: 657 },
+//     { title: 'Icon Set', image: 'categories-3.jpg', items: 83559 },
+//     { title: 'Web UI Kit', image: 'categories-4.jpg', items: 4500 }
+// ])
 
 </script>
 <template>
@@ -17,8 +33,8 @@ const cards = ref([
             Top Categories
         </h2>
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <template v-for="card in cards">
-                <Card :title="card.title" :items="card.items" :image="card.image" />
+            <template v-for="category in categories" :key="category.id">
+                <Card :title="category.name" :items="category.products_count" :image="category.thumbnails" />
             </template>
             <!-- <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
                 <div class="overflow-hidden border border-gray-200 rounded-xl">
