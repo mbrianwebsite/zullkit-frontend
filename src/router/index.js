ce8b136a646d2e3364d6e09d12552e1631c8c089
MBrian Website <muhammad.brian.27@gmail.com>
2022-12-15 10:29:01 +0700

    ADD SUCCESS VIEW

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from "../views/LoginView.vue";
 import RegisterView from "../views/RegisterView.vue";
 import DetailView from "../views/DetailView.vue";
 import PricingView from "../views/PricingView.vue";
+import SuccessView from "../views/SuccessView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: "/pricing",
       name: "pricing",
       component: PricingView,
+    },
+    {
+      path: "/success",
+      name: "success",
+      component: SuccessView,
     },
     // {
     //   path: '/about',
